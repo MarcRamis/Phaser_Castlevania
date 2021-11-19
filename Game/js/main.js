db7a7880310bb219cbfb214c756cf9c3b021f1c0
Marc Ramis <marc.ramis@enti.cat>
2021-11-19 12:40:08 +0100

    Maincharacter prefs cleaned

--- Game/js/main.js
+++ Game/js/main.js
@@ -4,15 +4,7 @@ var gamePrefs=
 
 var mainCharacterPrefs=
 {
-    right:false,
-    left:false,
-    up:false,
-    down:false,
-    canJump:true,
-    isInAir:false,
-    isCrouching:false,
     jumpForce:300,
-    rightSide: true,
     frameRate:5,
     speed:100
 }
