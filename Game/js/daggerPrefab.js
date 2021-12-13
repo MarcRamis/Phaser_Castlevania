f4a4ac10846bf0456648035b37b1643c04d5f12f
Marc Ramis <marc.ramis@enti.cat>
2021-12-13 12:40:41 +0100

    Dagger Implemented

---
+++ Game/js/daggerPrefab.js
@@ -0,0 +1,35 @@
+class daggerPrefab extends Phaser.GameObjects.Sprite
+{
+    constructor(_scene,_positionX,_positionY,_spriteTag,_direction)
+    {
+        super(_scene,_positionX,_positionY + 10,_spriteTag);
+        _scene.add.existing(this);
+        _scene.physics.world.enable(this);
+        this.setOrigin(0.5);
+        this.setScale(0.8);
+        this.body.allowGravity = false;
+        //this.body.collideWorldBounds = true;
+        this.direction = _direction;
+        this.doOnce = true;
+    }
+
+    preUpdate(time, delta)
+    {
+        super.preUpdate(time, delta);
+        
+        // Impulse
+        if (this.doOnce){
+            this.body.setVelocityX(90 * this.direction);
+            this.doOnce = false;
+        }    
+
+        // Flip
+        if (this.direction > 0)
+        {
+            this.flipX = false;  
+        }
+        else{
+            this.flipX = true;
+        }
+    }
+}
