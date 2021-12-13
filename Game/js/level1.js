e1d7a671ccfc46cfe57caa6eb0f93b1eea4b5060
Alex <alex.alcaide@enti.cat>
2021-12-13 12:43:49 +0100

    gitflow-feature-stash: AddingPlayer-Level1

--- Game/js/level1.js
+++ Game/js/level1.js
@@ -4,13 +4,34 @@ class level1 extends Phaser.Scene{
     }
     preload(){
         //this.load.setPath("assets/sprites/");
+        //---------MAP----------//
+        var rutaMapSheet = "assets/map/Sprite-Sheets/";
+        this.load.image('Level-1_TileSheet', rutaMapSheet + 'Castelvania-Sheet.png');
+        this.load.image('Lamp', rutaMapSheet + 'Lamp.png');
 
-        this.load.setPath("assets/map/Sprite-Sheets/");
-        this.load.image('Level-1_TileSheet','Castelvania-Sheet.png');
-        this.load.image('Lamp','Lamp.png');
+        var rutaMap = "assets/map/";
+        this.load.tilemapTiledJSON('level_1', rutaMap + 'Cstelvania_NES_Level-1.json');
 
-        this.load.setPath("assets/map/");
-        this.load.tilemapTiledJSON('level_1','Cstelvania_NES_Level-1.json');
+        //---------BACKGROUND----------//
+        this.cameras.main.setBackgroundColor("#4488AA");
+        var rutaImg = 'assets/img/';
+        var rutaImgWeapons = 'assets/img/weapons/';
+        var rutaSnd = 'assets/snd/';
+
+        this.load.spritesheet('player', rutaImg + 'maincharacter_anim.png', { frameWidth: 104, frameHeight: 35 });
+
+        //---------ENEMIES----------//
+
+        //---------ITEMS----------//
+        this.load.spritesheet('items', rutaImg + 'Items.png', { frameWidth: 16, frameHeight: 16 });
+
+        //---------WEAPONS----------//
+        //this.load.image('morningStar', rutaImgWeapons + 'MorningStar.png');
+        this.load.image('axe', rutaImgWeapons + 'Axe.png');
+        this.load.image('firebomb', rutaImgWeapons + 'FireBomb.png');
+        this.load.spritesheet('firebomb_fire', rutaImgWeapons + 'FireBomb_fire.png', { frameWidth: 16, frameHeight: 15 });
+
+        //---------AUDIO----------//
 
     }
     create(){
@@ -27,90 +48,103 @@ class level1 extends Phaser.Scene{
         this.stairsNextScene = this.map.createLayer('Stairs-ChangeScene','Level-1_TileSheet');
         this.doors = this.map.createLayer('Door','Level-1_TileSheet');
         this.map.createLayer('Lamps','Lamp');
-        //this.map.createFromObjects('Lamps', null, 'Lamp')
-        //this.map.createLayer('moss_left_layer','tileset_moss');
-        //this.map.createLayer('moss_right_layer','tileset_moss');
-        //this.map.createLayer('moss_bottom_layer','tileset_moss');
+
         //Indicamos las colisiones con paredes/suelo/techo
         this.map.setCollisionBetween(1,11,true,true,'Ground');
 
+        //Player
+        this.loadPlayerAnimations();
+        this.loadWeaponAnimations();
+        this.loadItemsSheet();
         
-        //Pintamos la puerta
-        /*
-        this.entry = this.physics.add.sprite(65,268,'entry');
-        this.entry.body.allowGravity = false;
-        this.entry.body.immovable = true;
-        this.entry.body.setSize(24,34,true).setOffset(4,6);
-        */
-        //Pintamos al hero
+        this.morningStar = new morningStarPrefab(this, 50, 100);
+        this.player = new playerPrefab(this, 50, 100, 'player');
         
-        //Ajustamos el tamaño del body del hero y recolocamos el punto de pivote para cuadrarlo
-  
-
-        //this.physics.add.collider(this.hero,this.entry);
-
-
-        //Creamos un listener para detectar colisiones entre el hero y las paredes
-       
-        //La camara sigue al jugador
-        //this.cameras.main.startFollow(this.hero);
-        //this.cameras.main.setBounds(0,0,gameOptions.level1Width, gameOptions.level1Height);
-
-        
-        //this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.setCollisions();
         
         
-
-        /*this.anims.create({
-            key:'run',
-            frames:this.anims.generateFrameNumbers('hero',{start:2,end:5}),
-            frameRate:10,
-            repeat:-1
-        });
-
-        this.anims.create({
-            key:'gemIdle',
-            frames:this.anims.generateFrameNumbers('gem',{start:0,end:4}),
-            frameRate:10,
-            repeat:-1
-        });
-
-        this.anims.create({
-            key:'jump',
-            frames:this.anims.generateFrameNumbers('jumper',{start:0,end:3}),
-            frameRate:10,
-            repeat:-1
-        });
-
-        this.anims.create({
-            key:'crawl',
-            frames:this.anims.generateFrameNumbers('slime',{start:0,end:3}),
-            frameRate:10,
-            repeat:-1
-        });*/
-
-        //Pintamos las gemas
-        
-        //Cargamos el enemigo
-        
-
-        //Cargamos el enemigo
-        
-        //Cargamos animación del jumper <-- Me la he llevado al constructor
-        //this.jumper.anims.play('jump',true);
-
-        //Cargamos la barra de energía
-        
-        //Pintamos la UI
     
     }
 
-    
+    loadPlayerAnimations() {
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNumbers('player', { frames: [0, 1, 2] }),
+            frameRate: mainCharacterPrefs.frameRate,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'normal_attack',
+            frames: this.anims.generateFrameNumbers('player', { frames: [12, 13, 14] }),
+            frameRate: mainCharacterPrefs.frameRateAttack,
+            repeat: 0
+        })
+        this.anims.create({
+            key: 'normal_large_attack',
+            frames: this.anims.generateFrameNumbers('player', { frames: [6, 7, 8] }),
+            frameRate: mainCharacterPrefs.frameRateAttack,
+            repeat: 0
+        })
+        this.anims.create({
+            key: 'special_attack',
+            frames: this.anims.generateFrameNumbers('player', { frames: [9, 10, 11] }),
+            frameRate: mainCharacterPrefs.frameRateAttack,
+            repeat: 0
+        })
+        this.anims.create({
+            key: 'crouch_attack',
+            frames: this.anims.generateFrameNumbers('player', { frames: [15, 16, 17] }),
+            frameRate: mainCharacterPrefs.frameRateAttack,
+            repeat: 0
+        })
+        this.anims.create({
+            key: 'crouch_large_attack',
+            frames: this.anims.generateFrameNumbers('player', { frames: [18, 19, 20] }),
+            frameRate: mainCharacterPrefs.frameRateAttack,
+            repeat: 0
+        })
+    }
+    loadWeaponAnimations() {
+        this.anims.create({
+            key: 'fireAnim',
+            frames: this.anims.generateFrameNumbers('firebomb_fire', { frames: [0, 1, 2, 3, 4] }),
+            frameRate: 4,
+            repeat: 0
+        })
+    }
+    loadItemsSheet() {
+        this.anims.create({
+            key: 'Little-Heart',
+            frames: this.anims.generateFrameNumbers('items', { frames: [0] }),
+            frameRate: 0,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'Heart',
+            frames: this.anims.generateFrameNumbers('items', { frames: [1] }),
+            frameRate: 0,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'MorningStar',
+            frames: this.anims.generateFrameNumbers('items', { frames: [2] }),
+            frameRate: 0,
+            repeat: -1
+        })
+    }
+    setCollisions() {
+        this.physics.add.overlap
+            (
+                this.player,
+                this.morningStar,
+                this.morningStar.playerCollided,
+                null,
+                this
+            );
+    }
 
     update(){
-        //Movimiento izquierda-derecha
-        
-        //Salto
+        this.player.Update();
         
     }
 }
