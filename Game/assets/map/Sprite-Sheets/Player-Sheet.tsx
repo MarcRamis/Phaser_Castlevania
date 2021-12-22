cca026fe255c33687591591eaffb904d322adce1
Alex Alcaide Arroyes <alex.alcaide@enti.cat>
2021-12-22 21:29:02 +0100

    Created SpawnPoints Between level-1 && level-2

---
+++ Game/assets/map/Sprite-Sheets/Player-Sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Player-Sheet" tilewidth="16" tileheight="32" tilecount="1" columns="1">
+ <image source="Player-Sheet.png" width="16" height="32"/>
+</tileset>
