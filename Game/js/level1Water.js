93d6b5292844c9a151137b1711cf76d53328e537
Marc Ramis <marc.ramis@enti.cat>
2021-12-30 17:45:28 +0100

    Load sounds & deleted what is not necessary

--- Game/js/level1Water.js
+++ Game/js/level1Water.js
@@ -1,45 +1,37 @@
-class level1Water extends Phaser.Scene{
-    constructor(){
-        super({key:"level1Water"});
+class level1Water extends Phaser.Scene {
+    constructor() {
+        super({ key: "level1Water" });
     }
-    preload(){
+    preload() {
         //---------PATHS----------//
         var rutaMap = "assets/map/";
         var rutaImg = 'assets/img/';
         var rutaImgWeapons = 'assets/img/weapons/';
         var rutaSnd = 'assets/snd/';
+        var rutaSndEffect = 'assets/snd/effects/';
 
         //---------MAP----------//
         var rutaMapSheet = "assets/map/Sprite-Sheets/";
         this.load.image('Castelvania-Sheet-Water', rutaMapSheet + 'Castelvania-Sheet-Water.png');
         this.load.tilemapTiledJSON('level_1Water', rutaMap + 'Castelvania_NES_Level-1_Water.json');
-        
+
         //---------BACKGROUND----------//
         this.cameras.main.setBackgroundColor("#4488AA");
-        
+
         //---------PLAYER----------//
         this.load.spritesheet('player', rutaImg + 'maincharacter_anim.png', { frameWidth: 104, frameHeight: 35 });
 
         //---------ENEMIES----------//
-        //Ghoul
-        this.load.spritesheet('enemyGhoul',rutaImg+'Enemy-Ghoul.png',
-        {frameWidth: 16, frameHeight: 32});
-        //Panther
-        this.load.spritesheet('enemyPanther',rutaImg+'Enemy-Panther.png',
-        {frameWidth: 32, frameHeight: 16});
-        //Bat
-        this.load.spritesheet('bat',rutaImg+'Enemy-Bat.png',
-        {frameWidth: 16, frameHeight: 14});
         //FishMan
-        this.load.spritesheet('fishMan',rutaImg+'Enemy-FishMan.png',{frameWidth:16, frameHeight:31});
+        this.load.spritesheet('fishMan', rutaImg + 'Enemy-FishMan.png', { frameWidth: 16, frameHeight: 31 });
 
-        this.load.image('shoot', rutaImg+'Shoot.png');
+        this.load.image('shoot', rutaImg + 'Shoot.png');
 
         //---------ITEMS----------//
         this.load.image('Lamp', rutaMapSheet + 'Lamp.png');
         this.load.spritesheet('items', rutaImg + 'Items.png', { frameWidth: 16, frameHeight: 16 });
-        this.load.spritesheet('lamp',rutaImg+'lamp.png',{frameWidth:10, frameHeight:16});
-        
+        this.load.spritesheet('lamp', rutaImg + 'lamp.png', { frameWidth: 10, frameHeight: 16 });
+
         //---------WEAPONS----------//
         this.load.image('dagger', rutaImgWeapons + 'Dagger.png');
         this.load.image('axe', rutaImgWeapons + 'Axe.png');
@@ -47,23 +39,31 @@ class level1Water extends Phaser.Scene{
         this.load.spritesheet('firebomb_fire', rutaImgWeapons + 'FireBomb_fire.png', { frameWidth: 16, frameHeight: 15 });
 
         //---------AUDIO----------//
+        this.load.audio('ost', rutaSnd + 'ost.mp3');
+        this.load.audio('hit', rutaSndEffect + 'hit.wav');
+        this.load.audio('takeItem', rutaSndEffect + 'takeItem.wav');
+        this.load.audio('crystalFire', rutaSndEffect + 'crystal.wav');
     }
-    create(){
-        
+    create() {
+
         // Animations
         this.loadPlayerAnimations();
         this.loadWeaponAnimations();
         this.loadItemsSheet();
         this.loadEnemyAnimations();
 
+        // Sounds
+        this.loadSounds();
+
         // Pools
         this.loadPools();
+
         // Map
-        this.loadMap();       
+        this.loadMap();
 
         // Player
         this.player = new playerPrefab(this, 50, 100, 'player');
-        
+
         // Utility
         this.setCamera();
         this.setCollisions();
@@ -149,8 +149,7 @@ class level1Water extends Phaser.Scene{
 
         });
     }
-    loadEnemyAnimations()
-    {
+    loadEnemyAnimations() {
         //Ghoul-Anim
         this.anims.create({
             key: 'ghoulWalk-Left',
@@ -206,7 +205,7 @@ class level1Water extends Phaser.Scene{
             frames: this.anims.generateFrameNumbers('bat', { start: 3, end: 5 }),
             frameRate: 7,
             repeat: -1
-        });  
+        });
         this.anims.create({
             key: 'batWalk-Left',
             frames: this.anims.generateFrameNumbers('bat', { start: 0, end: 2 }),
@@ -239,22 +238,13 @@ class level1Water extends Phaser.Scene{
             key: 'fishmanShoot-Left',
             frames: this.anims.generateFrameNumbers('fishMan', { start: 0, end: 0 }),
 
-        });   
+        });
     }
     setCollisions() {
-        this.map.setCollisionBetween(1,77,true,true,'Ground'); //Indicamos las colisiones con paredes/suelo/techo
+        this.map.setCollisionBetween(1, 77, true, true, 'Ground'); //Indicamos las colisiones con paredes/suelo/techo
         this.physics.add.collider(this.player, this.walls); // Ahora con el player
-        // this.physics.add.overlap
-        //     (
-        //         this.player,
-        //         this.morningStar,
-        //         this.morningStar.playerCollided,
-        //         null,
-        //         this
-        //     );
     }
-    loadMap()
-    {
+    loadMap() {
         //Pintamos el nivel
         //Cargo el JSON
         this.map = this.add.tilemap('level_1Water');
@@ -262,66 +252,71 @@ class level1Water extends Phaser.Scene{
         this.map.addTilesetImage('Lamp');
         this.map.addTilesetImage('Castelvania-Sheet-Water');
         //Pintamos las capas/layers
-        this.hud = this.map.createLayer('HUD','Castelvania-Sheet-Water');
-        this.walls = this.map.createLayer('Ground','Castelvania-Sheet-Water');
-        this.map.createLayer('BackGround','Castelvania-Sheet-Water');
-        this.stairs = this.map.createLayer('Stairs','Castelvania-Sheet-Water');
-        this.stairsNextScene = this.map.createLayer('Stairs-ChangeLevel','Castelvania-Sheet-Water');
+        this.hud = this.map.createLayer('HUD', 'Castelvania-Sheet-Water');
+        this.walls = this.map.createLayer('Ground', 'Castelvania-Sheet-Water');
+        this.map.createLayer('BackGround', 'Castelvania-Sheet-Water');
+        this.stairs = this.map.createLayer('Stairs', 'Castelvania-Sheet-Water');
+        this.stairsNextScene = this.map.createLayer('Stairs-ChangeLevel', 'Castelvania-Sheet-Water');
 
         // Leemos toda la información de las lámparas y enemigos en el mapa
         this.map.objects.forEach(layerData => {
-            switch(layerData.name)
-            {
-                case('Lamps-Obj'):
-                layerData.objects.forEach(lamp => {
-                    this.lamp = new lampPrefab(this,lamp.x + 8,lamp.y - 8,'lamp'); //Aqui se suman valores cambiarlo en el prefab directamente
-                    this.lamps.add(this.lamp);  
-                    this.lamp.body.allowGravity = false;
-                });
-                break;
-                case('Enemies'):
-                layerData.objects.forEach(enemy => {
-                    switch(enemy.properties[0].value)
-                    {
-                        case('Ghoul'):
-                        this.ghoul = new ghoulPrefab(this, enemy.x, enemy.y-64, 'enemyGhoul', 1);
-                        this.enemies.add(this.ghoul);
-                        this.ghoul.body.collideWorldBounds = true;
-                        break;
+            switch (layerData.name) {
+                case ('Lamps-Obj'):
+                    layerData.objects.forEach(lamp => {
+                        this.lamp = new lampPrefab(this, lamp.x + 8, lamp.y - 8, 'lamp'); //Aqui se suman valores cambiarlo en el prefab directamente
+                        this.lamps.add(this.lamp);
+                        this.lamp.body.allowGravity = false;
+                    });
+                    break;
+                case ('Enemies'):
+                    layerData.objects.forEach(enemy => {
+                        switch (enemy.properties[0].value) {
+                            case ('Ghoul'):
+                                this.ghoul = new ghoulPrefab(this, enemy.x, enemy.y - 64, 'enemyGhoul', 1);
+                                this.enemies.add(this.ghoul);
+                                this.ghoul.body.collideWorldBounds = true;
+                                break;
 
-                        case('Panther'):
-                        this.panther = new pantherPrefab(this, enemy.x, enemy.y-64, 'enemyPanther', -1);
-                        this.panthers.add(this.panther);
-                        this.panther.body.collideWorldBounds = true;
-                        break;
+                            case ('Panther'):
+                                this.panther = new pantherPrefab(this, enemy.x, enemy.y - 64, 'enemyPanther', -1);
+                                this.panthers.add(this.panther);
+                                this.panther.body.collideWorldBounds = true;
+                                break;
 
-                        case('Bat'):
-                        this.bat = new batPrefab(this, enemy.x, enemy.y, 'bat', -1);
-                        this.bats.add(this.bat);
-                        this.bat.body.collideWorldBounds = true;
-                        this.bat.Move(-1);
-                        this.bat.body.setGravity(0,-1000);
-                        break;
-                    }
-                });
-                break; 
+                            case ('Bat'):
+                                this.bat = new batPrefab(this, enemy.x, enemy.y, 'bat', -1);
+                                this.bats.add(this.bat);
+                                this.bat.body.collideWorldBounds = true;
+                                this.bat.Move(-1);
+                                this.bat.body.setGravity(0, -1000);
+                                break;
+                        }
+                    });
+                    break;
             }
         });
     }
-    setCamera()
-    {
+    loadSounds() {
+        this.ost = this.sound.add('ost');
+        this.ost.loop = true;
+        this.ost.play();
+
+        this.hit = this.sound.add('hit');
+        this.takeItem = this.sound.add('takeItem');
+        this.crystalFire = this.sound.add('crystalFire');
+    }
+    setCamera() {
         this.cameras.main.startFollow(this.player);
         this.cameras.main.setBounds(0, 0, gamePrefs.gameWidth, gamePrefs.gameHeight);
     }
 
-    loadPools()
-    {
+    loadPools() {
         this.enemies = this.physics.add.group();
         this.panthers = this.physics.add.group();
         this.bats = this.physics.add.group();
         this.lamps = this.physics.add.group();
     }
-    update(){
+    update() {
         this.player.Update();
     }
 }
